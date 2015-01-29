01f2a161a936649134cdf131da97f56ebf48f709
b3ans <b3ans_@hotmail.com>
2015-01-29 15:10:20 -0500

    Person.java

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    final int start;
+    final int dest;
+    final int time;
+    Person (int start, int dest, int time) {
+        this.start = start;
+        this.dest = dest;
+        this.time = time;
+    }
+}
