833eb9f1ce765f37541c8776abfba7948f2bcbdc
bporter2013 <brettporter123@gmail.com>
2015-02-03 11:34:33 -0500

    Building Class
    Taking in floors and creating their objects to be stored in a list

---
+++ Building.java
@@ -0,0 +1,37 @@
+package elevatorSimulator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Building {
+
+	public static void main(String[] args) {
+	
+		File file = new File("simulatorData.txt");
+		
+		ArrayList<Floor> floors = new ArrayList<>();
+		
+		// Checks for correct file name.
+		try {
+			
+			Scanner stdIn = new Scanner(file);
+			
+			// Takes first number of file.
+			// Which correlates to number of floors.
+			// Then creates floors and adds them to floors list.
+			for(int i = 0; i < stdIn.nextInt(); i++) {
+				Floor floor = new Floor();
+				floors.add(floor);
+			}
+			stdIn.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		for(int i = 0; i < floors.size(); i++) {
+			System.out.println(i);
+		}
+	}
+}
