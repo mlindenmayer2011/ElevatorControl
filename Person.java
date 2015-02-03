a1e3b7583a93cb5448152ed9283d02f432b6dc86
bporter2013 <brettporter123@gmail.com>
2015-02-03 11:52:59 -0500

    Person class
    Finished class. Put accessor and mutation methods. Also put it into the
    elevatorSimulator package which needs to be added to all classes so they
    can work together.

--- Person.java
+++ Person.java
@@ -1,10 +1,31 @@
+package elevatorSimulator;
+
 public class Person {
-    final int start;
-    final int dest;
-    final int time;
+
+    private int m_start, m_dest, m_time;
     Person (int start, int dest, int time) {
-        this.start = start;
-        this.dest = dest;
-        this.time = time;
+        this.m_start = start;
+        this.m_dest = dest;
+        this.m_time = time;
     }
+    
+    //Accessor Methods////////////////////////
+    public int getStart() { return m_start; }
+    public int getDest() { return m_dest; }
+    public int getTime() { return m_time; }
+    ///////////////////////////////////////////
+    
+    //Mutator Methods//////////////////////////
+    public void setStart( int start ) {
+    	m_start = start;
+    }
+    
+    public void setDest( int dest ) {
+    	m_dest = dest;
+    }
+    
+    public void setTime( int time ) {
+    	m_time = time;
+    }
+    ///////////////////////////////////////////
 }
