31fdef7bb740d1c1116fc53a25c60ac408011fdf
bporter2013 <brettporter123@gmail.com>
2015-02-03 20:22:44 -0500

    Change Person.java a little
    Used condition statements to get direction of person and removed mutator
    methods because we shouldn't have to change them ever.

--- Person.java
+++ Person.java
@@ -1,32 +1,27 @@
 package elevatorSimulator;
 
+//Used to store specified variables
+//that are required to move a person
+//correctly in an elevator.
 public class Person {
 
-    private int m_start, m_dest, m_time;
+    private int m_start, m_dest, m_time, m_dir;    
+    final int DOWN = -1, UP = 1;
     
     Person (int start, int dest, int time) {
-        this.m_start = start;
-        this.m_dest = dest;
-        this.m_time = time;
+        m_start = start;
+        m_dest = dest;
+        m_time = time;
+        
+        //Determines direction of person
+        if( start > dest ) { m_dir = DOWN; }
+        else               { m_dir = UP; }
     }
     
     //Accessor Methods////////////////////////
     public int getStart() { return m_start; }
-    public int getDest() { return m_dest; }
-    public int getTime() { return m_time; }
-    ///////////////////////////////////////////
-    
-    //Mutator Methods//////////////////////////
-    public void setStart( int start ) {
-    	m_start = start;
-    }
-    
-    public void setDest( int dest ) {
-    	m_dest = dest;
-    }
-    
-    public void setTime( int time ) {
-    	m_time = time;
-    }
+    public int getDest()  { return m_dest; }
+    public int getTime()  { return m_time; }
+    public int getDir()   { return m_dir; }
     ///////////////////////////////////////////
 }
