40c0044a6bef71ac250988153a2d9a30c1d2248a
b3ans <b3ans_@hotmail.com>
2015-02-17 12:46:38 -0500

    Added File to calc mean values

---
+++ Stats.java
@@ -0,0 +1,38 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Stats
+{
+
+    public static void main (String[] args)
+    {
+
+        double mu = 0;
+        int cnt = 0;
+        try {
+
+            Scanner in = new Scanner (new File("data.txt"));
+
+            in.nextInt();
+
+            while (in.hasNextLine()) {
+                int start = in.nextInt();
+                int dest = in.nextInt();
+                int time = in.nextInt();
+
+                //mu += time;
+                mu += dest;
+                cnt++;
+            }
+
+            in.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+            System.out.println(cnt + " | " + (double)mu/cnt);
+    }
+
+}
